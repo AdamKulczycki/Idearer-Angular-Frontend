a33244be4b2088e5185e27b10d6f8a08ffcba9ed
Adam <kulczyckiadam@gmail.com>
2018-10-26 17:58:32 +0200

    add possibility to add main comment to article

--- src/app/articles/article-item/article-detail/article-detail.component.ts
+++ src/app/articles/article-item/article-detail/article-detail.component.ts
@@ -4,6 +4,7 @@ import { Article } from '../../../models/article-model';
 import { Comment } from '../../../models/comment-model';
 import { ArticlesService } from '../../../services/articles.service';
 import { CommentsService } from '../../../services/comments.service';
+import { StorageService } from '../../../services/storage.service';
 
 @Component({
   selector: 'app-article-detail',
@@ -12,7 +13,14 @@ import { CommentsService } from '../../../services/comments.service';
 })
 export class ArticleDetailComponent implements OnInit {
 
-  constructor(private router: Router, private route: ActivatedRoute, private articlesService: ArticlesService, private commentsService: CommentsService) {
+  id: number;
+  article: Article;
+  comments: Comment[] = [];
+
+  constructor(private router: Router, private route: ActivatedRoute,
+    private articlesService: ArticlesService,
+    private commentsService: CommentsService,
+    private storageService: StorageService) {
     this.route.params.subscribe( (params: Params) => {
       this.id = params['id'];
       console.log(this.id);
@@ -32,10 +40,34 @@ export class ArticleDetailComponent implements OnInit {
         (error) => console.log(error)
     );
   }
+  answerClicked = false;
+  answerVisibility() {
+    this.answerClicked = !this.answerClicked;
+  }
 
-  id: number;
-  article: Article;
-  comments: Comment[] = [];
+  submitComment(form) {
+    const payload = {
+      article: {
+        id: this.article.id
+      },
+      content: form.value.comment
+    };
+
+    this.commentsService.makeComment(payload)
+    .subscribe(
+      (comment) => {
+        const commentItem = comment;
+        commentItem.user.username = this.storageService.get('username');
+        this.comments.push(commentItem);
+        this.answerClicked = false;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+
  /*  [new Comment(1, 'Babciu, to jest zajebiste!', '20/08/2018', 3312, 'wnusio', [new Comment(99, 'super', '20/08/2018', 231, 'babcia', [new Comment(1, 'tez sie ciesze babciu', '20/08/2018', 11, 'wnusio', [])]), new Comment(1, 'tez sie ciesze babciu', '20/08/2018', 11, 'wnusio', [])]),
   new Comment(2, 'Genialny przepis na ramen', '23/08/2018', 12, 'kucharczyk', []),
   new Comment(3, 'Skipnalem te czesc o gotowaniu i od razu przeszedlem do kapieli, swietnie dziala na skore. Polecam Skipnalem te czesc o gotowaniu i od razu przeszedlem do kapieli, swietnie dziala na skore. Polecam', '29/08/2018', 33, 'spa', []),
