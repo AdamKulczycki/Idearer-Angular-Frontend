3e2257692fe61bd648fca0f2716f3c6a82389bca
MrVaath <pat.wylegala@gmail.com>
2018-11-17 21:00:01 +0100

    Add sorting

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -20,6 +20,10 @@ export class ArticlesComponent implements OnInit {
   }
 
   articles: Article[] = [];
+  articlesList: Article[] = [];
+  sortDateReverse: boolean = false;
+  sortTitleReverse: boolean = false;
+  sortLikesReverse: boolean = false;
   category = '';
   isLogged: boolean;
 
@@ -31,6 +35,7 @@ export class ArticlesComponent implements OnInit {
           .subscribe(
             (articles) => {
               this.articles = articles;
+              this.articlesList = articles;
               console.log(articles);
             },
             (error) => console.log(error)
@@ -40,11 +45,51 @@ export class ArticlesComponent implements OnInit {
         .subscribe(
           (articles) => {
             this.articles = articles;
+            this.articlesList = articles;
           },
           (error) => console.log(error)
         );
       }
     });
+  }
+
+  public sortByDate(reverse: boolean) {
+    this.articlesList = this.articles.sort((first, second) => this.compareDate(first, second, reverse));
+    this.sortDateReverse = !this.sortDateReverse;
+  }
+
+  public sortByTitle(reverse: boolean) {
+    this.articlesList = this.articles.sort((first, second) => this.compareTitle(first, second, reverse));
+    this.sortTitleReverse = !this.sortTitleReverse;
+  }
+
+  public sortByLikes(reverse: boolean) {
+    this.articlesList = this.articles.sort((first, second) => this.compareLikes(first, second, reverse));
+    this.sortLikesReverse = !this.sortLikesReverse;
+  }
+
+  public compareDate(first: Article, second: Article, reverse: boolean) {
+    if (reverse) {
+      return (first.created <= second.created) ? 1 : 0;
+    } else {
+      return (first.created > second.created) ? 1 : 0;
+    }
+  }
+
+  public compareTitle(first: Article, second: Article, reverse: boolean) {
+    if (reverse) {
+      return (first.title <= second.title) ? 1 : 0;
+    } else {
+      return (first.title > second.title) ? 1 : 0;
+    }
+  }
+
+  public compareLikes(first: Article, second: Article, reverse: boolean) {
+    if (reverse) {
+      return (first.likesCount <= second.likesCount) ? 1 : 0;
+    } else {
+      return (first.likesCount > second.likesCount) ? 1 : 0;
+    }
   }
 
 }
