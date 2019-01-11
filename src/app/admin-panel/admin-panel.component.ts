bef3068a126c0d9eeb6c0baa079383bd5c180ef5
Adam <kulczyckiadam@gmail.com>
2019-01-11 21:36:29 +0100

    connect with server to reject report or accept with changing status

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -43,7 +43,8 @@ export class AdminPanelComponent implements OnInit {
                 (reports: Report[]) => {
                   this.reportsArray.push({
                     articleObject: article,
-                    show: false,
+                    showArticle: false,
+                    showPanel: false,
                     articleReports: reports
                   });
                   this.reportsNumber += reports.length;
@@ -60,39 +61,99 @@ export class AdminPanelComponent implements OnInit {
   articles = [];
   reportsArray = [];
   reportsNumber = 0;
-
   onSubmit(f, id) {
-    if (!f.value.reason) {
-      const payload = {
-        status: 'ACCEPTED'
-      };
-      this.articlesService.patchArticle(id, payload)
+    console.log(f.value);
+    // if (!f.value.reason) {
+    //   const payload = {
+    //     status: 'ACCEPTED'
+    //   };
+    //   this.articlesService.patchArticle(id, payload)
+    //     .subscribe(
+    //       res => console.log(res),
+    //       err => console.log(err)
+    //     );
+    // } else {
+    //   if (!f.value.otherReason) {
+    //     this.rejectsService.rejectArticle(id, f.value.reason)
+    //       .subscribe(
+    //         res => console.log(res),
+    //         err => console.log(err)
+    //       );
+    //   } else {
+    //     this.rejectsService.rejectArticle(id, f.value.otherReason)
+    //       .subscribe(
+    //         res => console.log(res),
+    //         err => console.log(err)
+    //       );
+    //   }
+    // }
+  }
+  onSubmitFromReportsPanel(f, id) {
+    const index = this.reportsArray.map(e => e.articleObject.id).indexOf(id);
+    if (!f.value.otherReason) {
+      this.rejectsService.rejectArticle(id, f.value.reason)
         .subscribe(
-          res => console.log(res),
+          res => {
+          console.log(res);
+          this.reportsNumber -= this.reportsArray[index].articleReports.length;
+          this.reportsArray.splice(index, 1);
+          },
           err => console.log(err)
         );
     } else {
-      if (!f.value.otherReason) {
-        this.rejectsService.rejectArticle(id, f.value.reason)
-          .subscribe(
-            res => console.log(res),
-            err => console.log(err)
-          );
-      } else {
-        this.rejectsService.rejectArticle(id, f.value.otherReason)
-          .subscribe(
-            res => console.log(res),
-            err => console.log(err)
-          );
-      }
+      this.rejectsService.rejectArticle(id, f.value.otherReason)
+        .subscribe(
+          res => {
+          console.log(res);
+          this.reportsNumber -= this.reportsArray[index].articleReports.length;
+          this.reportsArray.splice(index, 1);
+          },
+          err => console.log(err)
+        );
     }
   }
   viewSelectorChange(value) {
     this.viewSelector = value;
   }
   viewArticleChange(index) {
-    this.reportsArray[index].show = !this.reportsArray[index].show;
+    this.reportsArray[index].showArticle = !this.reportsArray[index].showArticle;
     console.log(this.reportsArray);
+  }
+  deleteAllreports(index) {
+    const numberOfReports = this.reportsArray[index].articleReports.length;
+    let reportsDeleted = 0;
+    // for (let i = this.reportsArray[index].articleReports.length - 1; i >= 0; i -= 1) {
+    //   reportsDeleted ++;
+    //   this.reportsService.deleteReport(this.reportsArray[index].articleReports[i].id)
+    //     .subscribe(
+    //       res => {
+    //         console.log(res);
+    //         reportsDeleted ++;
+    //         this.reportsArray[index].articleReports.splice(i, 1);
+    //         if (numberOfReports === reportsDeleted) {
+    //           console.log('All reports was deleted');
+    //           this.reportsNumber -= this.reportsArray[index].articleReports.length;
+    //           this.reportsArray.splice(index, 1);
+    //         }
+    //       },
+    //       err => console.log(err)
+    //     );
+    // }
+    this.reportsArray[index].articleReports.forEach(element => {
+      this.reportsService.deleteReport(element.id)
+        .subscribe(
+          res => {
+            console.log(res);
+            reportsDeleted ++;
+            if (numberOfReports === reportsDeleted) {
+              console.log('All reports was deleted');
+              this.reportsNumber -= this.reportsArray[index].articleReports.length;
+              this.reportsArray.splice(index, 1);
+            }
+          },
+          err => console.log(err)
+        );
+    });
   }
   ngOnInit() {
 
